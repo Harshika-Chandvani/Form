69f8bd391a6d529108267afb68eef3d1c16d618c
Harshika Chandvani <87865142+Harshika-Chandvani@users.noreply.github.com>
2021-09-20 21:05:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,62 +25,19 @@ function validcap() {
     }
 
 }
-// const startingMinutes = 10;
-// let time = startingMinutes * 60;
 
-// const countdownEl = document.getElementById('countdown');
-
-// setInterval(updateCountdown, 1000);
-
-// function updateCountdown() {
-//     const minutes = Math.floor(time / 60);
-//     let seconds = time % 60;
-
-//     seconds = seconds < 10 ? '0' + seconds : seconds;
-//     countdownEl.innerHTML = '$ {minutes}:  ${seconds}';
-//     time--;
-// }
-
-// // Set the date we're counting down to
-// var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-    
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-    
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-//   // Output the result in an element with id="demo"
-//   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-    
-//   // If the count down is over, write some text 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("countdown").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
 (function(endTime){
-    endTime*=1000; // javascript works with milliseconds
-    endTime+=new Date().getTime();// add the current system time into the equation
-    // your timeSync function
+    endTime*=1000; 
+    endTime+=new Date().getTime();
+    
     (function timerSync() {
-        // get elapsed time as a Date Object
+       
         var diff = new Date(endTime - new Date().getTime());
-        // get timer span
+       
         var timer=document.getElementById('countdown');
-        // has the timer finished?
+    
         if(diff<=0)return timerEnd(timer);
-        // set the current time difference
+        
         timer.innerHTML = 
         	doubleDigits(diff.getUTCHours())
         	+ ':' +
@@ -90,12 +47,11 @@ function validcap() {
         ;
         // recursion
         setTimeout(timerSync,1000);
-    })();// also call it to start the timer
-    // your timer end function
+    })();
 	function timerEnd(timer) {
         timer.innerHTML='Timer has stopped now';
     }
-    // double digit formatting
+   
     function doubleDigits(number) {
     	return number<10
         	? '0'+number
@@ -104,4 +60,4 @@ function validcap() {
     } 
 })(
     (0 * 0)+ (3 * 60) + (00)
-);// time to elapse 1:30:10
+);
